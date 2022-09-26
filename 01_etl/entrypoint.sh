128718fb654dbbdcc643d8a6da69c7202b6129b5
wood <amper.sil.tok@gmail.com>
2022-09-26 11:15:44 +0300

    добавлено создание индекса при запуске контейнера

--- 01_etl/entrypoint.sh
+++ 01_etl/entrypoint.sh
@@ -166,4 +166,75 @@ curl -XPUT http://elasticsearch:9200/genres -H 'Content-Type: application/json' 
   }
 }'
 
+curl -XPUT http://elasticsearch:9200/persons -H 'Content-Type: application/json' -d'
+{
+  "settings": {
+    "refresh_interval": "1s",
+    "analysis": {
+      "filter": {
+        "english_stop": {
+          "type":       "stop",
+          "stopwords":  "_english_"
+        },
+        "english_stemmer": {
+          "type": "stemmer",
+          "language": "english"
+        },
+        "english_possessive_stemmer": {
+          "type": "stemmer",
+          "language": "possessive_english"
+        },
+        "russian_stop": {
+          "type":       "stop",
+          "stopwords":  "_russian_"
+        },
+        "russian_stemmer": {
+          "type": "stemmer",
+          "language": "russian"
+        }
+      },
+      "analyzer": {
+        "ru_en": {
+          "tokenizer": "standard",
+          "filter": [
+            "lowercase",
+            "english_stop",
+            "english_stemmer",
+            "english_possessive_stemmer",
+            "russian_stop",
+            "russian_stemmer"
+          ]
+        }
+      }
+    }
+  },
+  "mappings": {
+    "dynamic": "strict",
+    "properties": {
+      "id": {
+        "type": "keyword"
+      },
+      "name": {
+        "type": "keyword"
+      },
+      "role": {
+        "type": "keyword"
+      },
+      "filmworks": {
+        "type": "nested",
+        "dynamic": "strict",
+        "properties": {
+          "id": {
+            "type": "keyword"
+          },
+          "filmwork": {
+            "type": "text",
+            "analyzer": "ru_en"
+          }
+        }
+      }
+    }
+  }
+}'
+
 python3 main.py
